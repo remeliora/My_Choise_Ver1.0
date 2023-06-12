498a8a9a13d6c5b6d0848bab226fc5100067a99f
remeliora <remeliora2020@gmail.com>
2023-06-12 12:53:52 +1000

    first commit

---
+++ MC_VER3/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MC_VER3
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Close_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Opacity -= 0.1;
+                await Task.Delay(2);
+            }
+            this.Close();
+        }
+
+        private void Button_Hide_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+    }
+}
---
+++ MC_VER3/View/Tasks.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace MC_VER3.View
+{
+    /// <summary>
+    /// Логика взаимодействия для Tasks.xaml
+    /// </summary>
+    public partial class Tasks : UserControl
+    {
+        public Tasks()
+        {
+            InitializeComponent();
+        }
+    }
+}
